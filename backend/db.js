90cdd484733cdfc991cc1babf2f229380e325d20
ayushkanani711 <ayushkanani2003@gmail.com>
2023-11-15 22:15:40 +0530

    improve security

--- backend/db.js
+++ backend/db.js
@@ -2,13 +2,6 @@ const mongoose =require('mongoose');
 require('dotenv').config();
 const mongoURL = process.env.mongoURL
 
-//ULR of mongoDB compass
-// const mongoURI = "mongodb://localhost:27017/inotebook"
-
-//ULR of mongoDB Atlas
-// const mongoURL = "mongodb+srv://ayushkanani711:SowhWPNoyRvHb4FF@cluster0.k7qqbvc.mongodb.net/iNotebook"
-
-
 const connetToMongo = async () => {
         try {
             await mongoose.connect(mongoURL, {
--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,6 @@ connectToMongo();
 
 app.use(cors())
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
 
 // Avilabel All Routes
 app.use('/api/auth' , require('./routes/auth'))
