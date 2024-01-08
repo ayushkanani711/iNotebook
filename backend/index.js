1a6893d37f49d18c42f1aac30ea8b5c156858570
ayushkanani711 <ayushkanani2003@gmail.com>
2024-01-08 16:34:40 +0530

    remove bug from App.js

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,20 @@
-const connectToMongo =  require('./db')
-const express = require('express')
-const cors = require('cors')
+const connectToMongo = require("./db");
+const express = require("express");
+const cors = require("cors");
 
-
-const app = express()
-const port = 4000
+const app = express();
+const port = 4000;
 
 //Connect to mongoDB
 connectToMongo();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 // Avilabel All Routes
-app.use('/api/auth' , require('./routes/auth'))
-app.use('/api/notes' , require('./routes/notes'))
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-  console.log(`\niNotbook app listening on port ${port}`)
-})
+  console.log(`\niNotbook app listening on port ${port}`);
+});
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
-import NoteState from "./context/notes/NOtesState";
+import NoteState from "./context/notes/NotesState";
 import Alert from "./components/Alert";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
